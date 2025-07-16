8031fd75797acd657ea6f4421038117d2e5044b8
Shannon <sestumpf@gmail.com>
2025-07-15 17:51:11 -0700

    Finished with DP album covers.  Next up is Rainbow covers

--- mark6.php
+++ mark6.php
@@ -17,12 +17,12 @@
 </figure>
   
 <!--Purpendicular-->
-<h3>Purpendicular - <em>1996</em></h3><p> <img class="album_cover" src="imgs/Purpendicual Cover.jpeg" alt="Purpendicular Cover"/></p>
+<h3>Purpendicular - <em>1996</em></h3><p> <img class="album_cover" src="imgs/cover_purpendicular.png" alt="Purpendicular Cover"/></p>
 <p>From this point forward, Deep Purple (which always seemed like Ritchie's band) will feel much like an Ian Gillan solo project. 'Purpendicular,' doesn't launch the Steve Morse era off to a very promising start.  A gifted, technical guitar player, his style is a departure from Ritchie, and with his signature artificial harmonics and ultra clean, ultra fast speed bursts it's obvious that this is never going to be the classic Deep Purple sound.  With Ritchie, even when the albums were sub par you could put a great solo on each song in the bank.  But after the 'Battle Rages On', it was obvious that Ritchie's genius no longer outweighed his petulance.  Sadly, it wouldn't be the same band without him.  There's no need to disparage the Morse era, however, as it is further evidence that Deep Purple is never the same band for long and the different lineups have taught us that the only musical style that is constant with this hard rock band, is that nothing will ever be constant.  The album starts off great with 'Ted the Mechanic' and 'Loosen My Strings', the first raucous and rocking and the second a cool, bluesy and soulful number with an interesting ascending chord progression.  There are nice moments sprinkled throughout and the musical range is evident when you consider the bona fide celtic reading of 'Cascades' but overall the songs sound generic and boring.  A sign of life finally pops up towards the end with 'Somebody Stole My Guitar' but ultimately, clocking in at close to 60 minutes this album suffers from the curse of the CD era, it's just way too long.  Editing the material down to say, 35 minutes or so might have made this album flow a lot better and helped it's rating.
 <span class="yellow"> (rating: 2/5 whammy bars)</span></p>
 
 <!--Abandon-->
-<h3>Abandon - <em>1998</em></h3><img class="album_cover" src="imgs/Abandon.jpg" alt="Abandon Cover"/>
+<h3>Abandon - <em>1998</em></h3><img class="album_cover" src="imgs/cover_abandon.png" alt="Abandon Cover"/>
 <p>Steve Morse's style is more reminiscent of Gillan's old hot shot guitar player Janick Gers than Blackmore's which strengthens the comparison between Morse era Deep Purple and Ian Gillan's solo work. That's not all bad, Gers was a shredder of epic proportions.  'Abandon' is a diverse enough album, stylistically, with a few good ballads mixed in with heavier sounding tracks.  A bit of prog is sprinkled in as well, with uneven, but interesting results and a handful of truly great moments along the way.
 <span class="yellow"> (rating: 3/5 whammy bars)</span></p>
 
--- mark8.php
+++ mark8.php
@@ -23,6 +23,9 @@
   <span></span>
 </div>
 
+<!-- =1 -->
+<h3>=1 - <em>2020</em></h3><img class="album_cover" src="imgs/cover_=_1.png" alt="Whoosh"/><p>Is this the last Deep Purple studio album?  If it is, they made a pretty cool move in covering 'And the Address' to close out their career since it is the first song on their first album 'Shades of Deep Purple.'  Overall, however, this album is middle of the road.  'Whoosh' suffers from the same mistake so many artists have made since the advent of CD's;  what to do with all of that extra time available?  Vinyl was a temporally perfect format because of it's forced constraint, afterall an engineer could only cram so much material onto an LP, usually 40-45 minutes and this of course, forced all of the creative powers to make tough decisions regarding which material to include. 'Whoosh' is too long.  Ian Paice and Roger Glover prove for the umpteenth time why those two are so great together and Don Airey has definitely become the focus of the band with his amazing chops, melodic ideas, riffiness and expert soloing.  Ian Gillan? Well, his vocals ain't what they used to be but has always had a great imagination and creativity and this album is no different.  Morse is a brilliant, technical guitarist at times although he's guilty of recycling guitar solos and writing is not his strength.  Overall, this is just another average post Blackmore, post Lord album.  Nothing is truly terrible on here but nothing really stands out either.  If you're looking for a reminder of the glory days of Deep Purple, you won't find it here.  But if you're like me, and you just really love these guys and are interested in following their careers, you'll find plenty to enjoy on this record.
+<span class="yellow"> (rating: 2.5/5 whammy bars)</span>
 
 
 <?php include 'includes/footer.php'; ?>
