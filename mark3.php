67de5ce40708b0bd6849ae09c1c073a4b8863493
Shannon <sestumpf@gmail.com>
2025-07-21 15:21:30 -0700

    mark 3 new photo, html and css tweaks

--- mark3.php
+++ mark3.php
@@ -5,51 +5,49 @@
 </head>
    
    
-<body>
+<body class ='body_review_pages'>
     <?php include 'includes/header.php'; ?>
 
+<h1 id='h1'>Mark 3 with new members David Coverdale and Glenn Hughes</h1>
 
 
-<!--Mk 3-->
-<h2>Mk III</h2>
-<figure class='mkIII'>
-<div> 
-  <img src="imgs/JLCJ.jpg" alt="Jon Lord"/>
-  <img src="imgs/GHCJ.jpg" alt="Glenn Hughes"/>
-  <img src="imgs/IPCJ.jpg" alt = "Ian Paice"/>
-  <img src="imgs/DCCJ.jpg" alt = "David Coverdale"/>
-  <img src="imgs/RBCJ.jpg" alt = "Ritchie Blackmore"/>
-</div>
-<figcaption><span class="lineup">New Legendary Lineup:</span> Jon Lord, Glenn Hughes (vocals, bass), Ian Paice, David Coverdale (vocals), Ritchie Blackmore (1973-1975)</figcaption>
+<figure>
+<img src="imgs/mark3_group_photo.png" alt="a group photo of the original Deep Purple lineup with Simper and Evans">
+  <figcaption>
+    <span class="caption-title">Hughes, Coverdale era:</span> 
+    <br>
+    Jon Lord, Glenn Hughes (vocals, bass), Ian Paice, David Coverdale (vocals), Ritchie Blackmore (1973-1975)
+  </figcaption>
 </figure>
 
 <!--Burn-->
 <h3 class='album_title_header'>Burn - <em>1974</em></h3><p><img class="album_cover" src="imgs/cover_burn.png" alt="Burn Cover"/>
 <p class='review_text'>
 Ritchie Blackmore has always had a knack for finding unknown talent so it's no surprise that he championed and helped cultivate a superstar by auditioning David Coverdale.  Before 'Burn', Gillan had resigned and this opened the door for Glover to be (albeit rudely) cut from the lineup as well.  All of this worked in Ritchie's favor, of course,  so that he could move the band in a blusier direction. Thus, Deep Purple welcomed Glenn Hughes (an amazing singer and naturally gifted bass player) along with David Coverdale.  Glenn's stratospheric singing approach made sure that the vocal range of the band would still match that of the departed Gillan and as an added bonus, vocal harmonies could take a much more prominent role in the singing. Often Hughes and Coverdale shared lead vocal duties on verses and sometimes doubled each other up, always to great effect.  Ritchie, having gotten his way, was happy for at least an album and he refreshed exuberance shows up in his riffs, solos and songwriting.  When Ritchie takes the time to detail and layer up his guitar solos, as he does on 'Burn', it's obvious that he's in a good place creatively.  'Burn,' (the title track) with it's ambitious song structure, flows effortlessly thanks to the reliable and incredible breaks and transitions of Ian Paice.  This album opener is on par with 'Highway Star' (another legendary track to open an album) and jump starts the new Mk III era quite impressively.  'Lay Down Stay Down' has a great riff and a stop and start rhythm.  Hughes' vocals effortlessly blends R&B and hard rock, adding in very nice dual harmonies.  'Sail Away' contains another of the album's monster riffs and features great tag team interaction between the Coverdale and Hughes. Jon Lord's tone and synth choice is out of this world, where is this sailboat you're on heading to, exactly, Mars?  'Mistreated' remained a staple in Ritchie and David's repertoire long after Deep Purple split up, a testament to the appeal and importance of the song.  And just when you think the reformed Deep Purple is funkier and blusier and earthier, they take you on a space ride with A200 to close out one of their best albums.
-<span class="yellow"> (rating: 5/5 whammy bars)</span></p><p>Songs that didn't make the album that you should definitely listen to:<b> Coranarias Redig</b>
 </p>
+<p><span class="album_rating"> (rating: 5/5 whammy bars)</span></p></p>
+<p>🎧 Songs that didn't make the album that you should definitely listen to:<b> Coranarias Redig</b></p>
 
 <!--Live in London--> 
 <h3 class='album_title_header'>Live in London<em>1974 - (released in 1982)</em></h3><img class="album_cover" src="imgs/cover_live_in_london.png" alt="Live In London Cover"/>  
 <p class='review_text'>
 This is the MK 3 version of 'Made In Japan', even borrowing a couple of tunes from the MK 2 catalogue; the obligatory 'Smoke on the Water' and 'Space Truckin.'  'Smoke on the Water' is a fantastic version, in part due to the extraordinary harmonies of Coverdale and Hughes, which gives the well worn song a fresh interpretation complete with the giant Hughes credenza right before the end, where he engages in some R&B scatting, showing off his range, right before the thrilling conclusion.  Throughout, Deep Purple is firing on all cylinders, relying heavily on material from the 'Burn' album.  Blackmore is inspired and plays blistering leads, throughout.  Jon Lord introduces each band member during the proceedings and ironically, the biggest cheer from the crowd is for Ritchie, cementing the fact that he was the life and soul of Deep Purple.  Soon, he would have one foot out the door.  No matter, that would be a concern for the future.  For now, all of these songs arguably sound even better and more exciting than their studio counterparts.  The lineup may have been shuffled, but the results where still legendary.  
-<span class="yellow"> (rating: 5/5 whammy bars)</span>
 </p>
+<p><span class="album_rating"> (rating: 5/5 whammy bars)</span></p>
 
 <!--Stormbringer-->
 <h3 class='album_title_header'>Stormbringer - <em>1974</em></h3><p><img class="album_cover" src="imgs/cover_stormbringer.png" alt="Stormbringer Cover"/> 
 <p class='review_text'>
 Somewhat controversial among Deep Purple fans, this album is not as "metal" their earlier albums. In fact, at times the songs are more chilled-out R&B than rock. But don't despair, alongside the quieter moments, there is plenty of hard rock on this album.  Deep Purple, never a band to be strictly beholden to a particular genre, demonstrate their versatility and show their creative restlessness.  Coverdale and Hughes are allowed more room to manuever on this album and as a result produce songs that are true high points for their careers.  Ritchie, for his part, seems to have checked out a bit on some of the songs (like he did on 'Who Do  We Think We Are') but on this album, other members rush in to fill the vacuum.  Blackmore's diminished presence allows Jon Lord a lot of extra room to manuever and contribute, to very nice effect.  Lord seemed to prefer the background after 'Machine Head' which is a shame, because it all sounds so much more Purply when he indulges his creative spirit.  When Blackmore CAN be bothered, he shows up fiercely and proves that he has plenty of fire left in his Stratocaster.  Unfortunately, for the most part it seems he was distracted, daydreaming about Ronnie James Dio and a soon to be unveiled Rainbow project.  Track highlights:  'Stormbringer':  rockin' with tremendous slide guitar work and wicked wah drenched Hammond B2.  Funky, hard and heavy, a great opener.  'Lady Double Dealer,'  'Highball Shooter,' 'The Gypsy' and 'Soldier of Fortune.'
-<span class="yellow"> (rating: 4/5 whammy bars)</span>
 </p>
+<p><span class="album_rating"> (rating: 4/5 whammy bars)</span></p>
 
 <!--Graz 1975-->
 <h3 class='album_title_header'>Graz - <em>1975 (released 2014)</em></h3><img class="album_cover" src="imgs/cover_graz.png" alt="Graz Cover"/> 
 <p class='review_text'>
 Here is the best counterpart to 'Live In London' you're likely going to find. This live recording is taken from the 'Stormbringer' tour and is culled from the live concert three days before Ritchie left Deep Purple to start Rainbow with Ronnie James Dio.  This album is essential because where else are you going to find a live cut of 'Stormbringer,' immediately after a rollicking version of 'Burn?'  Additionally, we get to hear first-rate renditions of 'Gypsy' and 'Lady Double Dealer.'  The overblown rendition of 'Space Truckin', (now that we have already heard it on 'Live In London') does tend to drag on a bit but it's still worth it since we don't want to miss a single note from this legendary band burning along at their prime.
-<span class="yellow"> (rating: 5/5 whammy bars)</span>
 </p>
+<p><span class="album_rating"> (rating: 5/5 whammy bars)</span></p>
 
 <!-- NAVIGATION THROUGH THE MK LINEUPS BOTTOM OF PAGE -->
 <div class="lineup-nav">
