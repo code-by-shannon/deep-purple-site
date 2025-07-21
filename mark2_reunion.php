ffc5ced69f1fc4ddda66dd32962fed77b6953990
Shannon <sestumpf@gmail.com>
2025-07-21 15:40:00 -0700

    html, photo and css tweaks for mark 3, 4 and reunion

--- mark2_reunion.php
+++ mark2_reunion.php
@@ -4,33 +4,36 @@
 <?php include 'includes/head.php'; ?>
 </head>
     
-   
-
 <!-- BODY -->
-<body>
-    <?php include 'includes/header.php'; ?>
+<body class = 'body_review_pages'>
+<?php include 'includes/header.php'; ?>
 
-<!--MK 2 Reunion-->
-<h2>Mk II - Reunited</h2>
+
+<h1 id='h1'>Mark II Reunited</h1>
 <figure>
-    <img class="jlt" src="imgs/DP PS.jpeg" alt="MK II Reunited">
-    <figcaption class="fig">
-    <span class="lineup">Together again!:</span> Gillan, Lord, Paice, Glover and Blackmore</figcaption>
+  <img src="imgs/mark2_reunion_group_photo.png" alt="a group photo of mark 2 lineup reunited">
+  <figcaption>
+    <span class="caption-title">Mark 2 reunited:</span>
+    <br>
+    Ian Gillan, Jon Lord, Ian Paice, Roger Glover, Ritchie Blackmore
+  </figcaption>
 </figure>
 
 <!--Perfect Strangers-->
 <h3 class='album_title_header'>Perfect Strangers - <em>1984</em></h3>
 <p class='review_text'>
 <img class="album_cover" src="imgs/cover_perfect_strangers.png" alt="Perfect Strangers Cover"/>
-The long awaited and anticipated reunion of not only Deep Purple but the venerated MK II lineup was not a disappointment.  It's obvious that, at least briefly, all members were excited to be back together again.  Jon Lord circles the waters like a shark on the prowl, introducing the album.  When Blackmore is enthusiastic and in good spirits, it shows in his playing and on this album, his aggressive and playful guitar work is on full display.  He doesn't let up on the throttle for rockers like 'Knocking at Your Backdoor,' or 'Gypsy's Kiss' or the eastern influenced, odd rhythm hypnosis of 'Perfect Strangers.' For good measure, Ritchie checks in with a brilliant solo on the slow burning 'Wasted Sunsets.'  Sadly, this is the last time you will hear Jon Lord really stretch out on keyboards in such an exciting manner and the duels between he and Blackmore harken back to the glory days of the MK II era in genuine fashion.  Ian Gillan, also, is in fine form, scatting and screeching with abandon, his lyrics fun and enticing and as always, nonsensical, especially on the goofy but enticing 'Gypsy's Kiss.'  This would be the last truly great Deep Purple album.  This isn't 'Machine Head', 'In Rock' or 'Burn' but instead something different than all of those but in it's own way almost as interesting.  This is also the last album by Deep Purple that that feels restless, and goads their imaginations into creating something surprising and unique.<span class="yellow"> (rating: 4/5 whammy bars)</span>
+The long awaited and anticipated reunion of not only Deep Purple but the venerated MK II lineup was not a disappointment.  It's obvious that, at least briefly, all members were excited to be back together again.  Jon Lord circles the waters like a shark on the prowl, introducing the album.  When Blackmore is enthusiastic and in good spirits, it shows in his playing and on this album, his aggressive and playful guitar work is on full display.  He doesn't let up on the throttle for rockers like 'Knocking at Your Backdoor,' or 'Gypsy's Kiss' or the eastern influenced, odd rhythm hypnosis of 'Perfect Strangers.' For good measure, Ritchie checks in with a brilliant solo on the slow burning 'Wasted Sunsets.'  Sadly, this is the last time you will hear Jon Lord really stretch out on keyboards in such an exciting manner and the duels between he and Blackmore harken back to the glory days of the MK II era in genuine fashion.  Ian Gillan, also, is in fine form, scatting and screeching with abandon, his lyrics fun and enticing and as always, nonsensical, especially on the goofy but enticing 'Gypsy's Kiss.'  This would be the last truly great Deep Purple album.  This isn't 'Machine Head', 'In Rock' or 'Burn' but instead something different than all of those but in it's own way almost as interesting.  This is also the last album by Deep Purple that that feels restless, and goads their imaginations into creating something surprising and unique.
 </p> 
+<p><span class="album_rating"> (rating: 4/5 whammy bars)</span></p>
 
 <!--House of Blue Light-->
 <h3 class='album_title_header'>House of Blue Light <em>- 1987</em></h3>
 <p class='review_text'>
 <img class="album_cover" src="imgs/cover_house_blue_light.png" alt="House of Blue Light Cover"/>
-This album is glossy in it's production, like Joe Lynn Turner era Rainbow.  Although the intent is to deliver accessible pop, the musicianship is, of course, exceptional, throughout.  It's quite short on progressive ideas although there are a few here and there.  The highlights are 'Bad Attitude,' and 'Mad Dog' but 'Spanish Archer' is the real standout track with it's medieval guitar opening and a relentless exciting ambience that intensifies Gillan's captivating and powerful lyrics.  It's disappointing that Jon takes a back seat throughout almost the entirety of this album, because, it just isn't classic Deep Purple without the duels between Ritchie's Stratocaster and Jon's Hammond B2.  Every Deep Purple album is better when Lord is given a chance to shine.  On the bright side, many of the tunes are riffy enough but ultimately, there are too many slow and dull moments, most heinous of all is a recycled Rainbow riff that pops up in it's 3rd or 4th iteration.  This record fails because largely because it's a product of intentionally aiming for pop success.  The result is pop that isn't good enough to be truly popular while the bands strengths are watered down when they should be showcased.<span class="yellow"> (rating: 2.5/5 whammy bars)</span>  
+This album is glossy in it's production, like Joe Lynn Turner era Rainbow.  Although the intent is to deliver accessible pop, the musicianship is, of course, exceptional, throughout.  It's quite short on progressive ideas although there are a few here and there.  The highlights are 'Bad Attitude,' and 'Mad Dog' but 'Spanish Archer' is the real standout track with it's medieval guitar opening and a relentless exciting ambience that intensifies Gillan's captivating and powerful lyrics.  It's disappointing that Jon takes a back seat throughout almost the entirety of this album, because, it just isn't classic Deep Purple without the duels between Ritchie's Stratocaster and Jon's Hammond B2.  Every Deep Purple album is better when Lord is given a chance to shine.  On the bright side, many of the tunes are riffy enough but ultimately, there are too many slow and dull moments, most heinous of all is a recycled Rainbow riff that pops up in it's 3rd or 4th iteration.  This record fails because largely because it's a product of intentionally aiming for pop success.  The result is pop that isn't good enough to be truly popular while the bands strengths are watered down when they should be showcased.
 </p>
+<p><span class="album_rating"> (rating: 2.5/5 whammy bars)</span>  </p>
 
 <!-- NAVIGATION THROUGH THE MK LINEUPS BOTTOM OF PAGE -->
 <div class="lineup-nav">
--- mark4.php
+++ mark4.php
@@ -3,28 +3,31 @@
 <head>
   <?php include 'includes/head.php'; ?>
 </head>
-    <?php include 'includes/head.php'; ?>
+   
     
-
-
-<body>
+<body class='body_review_pages'>
     <?php include 'includes/header.php'; ?>
 
-<!--MK 4-->
+
  
-<h2>Mk IV</h2>
+<h1 id='h1'>Tommy Bolin Era</h1>
 <figure>
-    <img class="jlt" src="imgs/DPMK4.jpg" alt="MK IV"/> 
-    <figcaption class="fig"><span class="lineup">Enter Tommy Bolin:</span> David Coverdale, Ian Paice, Jon Lord, Tommy Bolin (guitar), Glenn Hughes (1975-1976)</figcaption>
+  <img src="imgs/mark4_group_photo.png" alt="a group photo with new member Tommy Bolin">
+  <figcaption>
+    <span class="caption-title">Mark IV Lineup:</span>
+    <br>
+    David Coverdale, Ian Paice, Jon Lord, Tommy Bolin, Glenn Hughes (1975-1976)
+  </figcaption>
 </figure>
+
 
 <!--Come Taste the Band-->
 <h3 class='album_title_header'>Come Taste the Band - <em>1975</em></h3>
 <p class='review_text'>
 <img class="album_cover" src="imgs/cover_come_taste_the_band.png" alt="Come Taste the Band Cover"/> 
 Another major lineup change and another great album.  Somehow, this isn't shocking, Deep Purple always seems to find a way.  This time, the one to leave is Ritchie who went on to form Rainbow with Ronnie James Dio.  So how do you replace a guitar virtuoso who's fretboard work was the primary force behind the band?  Well, you just go get Tommy Bolin.  Seriously, what other guitarist could have come in and done that?  Unlike the classic MK II era, this band is funkier and more R&B driven, but it still sounds wild and exotic with Bolin's exceptional playing throughout.  The Glenn Hughes songs are the most satisfying i.e. 'Gettin' Tighter' and 'This Time Around,' the former with it's heavy, funky riff and jazzy middle section, the latter a gorgeous and inventive masterpiece of song development.  And man, can Glenn Hughes ever sing!!  The Coverdale songs tend to foretell the vibe of the upcoming Whitesnake era, bluesier, grittier but a little short on melody i.e. 'I Need Love,' 'Drifter,' and 'Lady Luck' brings the album down a star.  Coverdale does redeem himself however, with the closer 'You Keep On Movin,' which is sublime and elevated blues.
-  <span class="yellow"> (rating: 4/5 whammy bars)</span> 
 </p>
+<p><span class="album_rating"> (rating: 4/5 whammy bars)</span> </p>
 
 
 <!--Long Beach 1976-->
@@ -32,8 +35,8 @@ Another major lineup change and another great album.  Somehow, this isn't shocki
 <p class='review_text'>
 <img class="album_cover" src="imgs/cover_long_beach.png" alt="Live 1976 Cover"/> 
 First of all, relegating Tommy Bolin to rank of cover guitarist, while unavoidable in this situation, is difficult to listen to.  Blackmore, by this point had played all of these songs so brilliantly on previous live versions that no one in the universe could step in and do them justice.  Bolin was a gamer, and it goes without saying, a brilliant guitarist in his own right, but listening to him try to paint by number his way through classic Deep Purple material (let alone on a Les Paul, egad!"  well, it's nowhere near as satisfying as hearing the live renditions of the material from 'Come Taste the Band'.  'Smoke on the Water' is a blessing and a curse for this band as obviously it and the other classics had to be played to placate the audience.  But the MK IV material is much more satisfying to listen to on this album.  Still, anything with Bolin on it, due to his short career, is essential.  After this tour, or part way through it, drugs would eat away at the heart and soul of this short lived lineup, leaving behind all of the untapped potential.  In fact, Deep Purple would remain quiet for 8 long years afterwards . . .
-<span class="yellow"> (rating: 4/5 whammy bars)</span>
 </p>
+<p><span class="album_rating"> (rating: 4/5 whammy bars)</span></p>
 
 <!-- NAVIGATION THROUGH THE MK LINEUPS BOTTOM OF PAGE -->
 <div class="lineup-nav">
