14955af4e059af450a797f73659c00fa3d6e416d
Shannon <sestumpf@gmail.com>
2025-07-28 12:52:04 -0700

    review for =1 added

--- mark8.php
+++ mark8.php
@@ -29,8 +29,8 @@
 <h3 class='album_title_header'>=1 - <em>2020</em></h3>
 <p class='review_text'>
 <img class="album_cover" src="imgs/cover_=_1.png" alt="Whoosh"/>
-Review coming soon</p>
-<span class="rating"> (rating: /5 whammy bars)</span>
+We enter the Simon McBride era.  Steve Morse called it a career after approximately 30 years with the band.  Simon McBride is exactly what you would expect.  Deep Purple would have the pick of the litter available to them and of course, McBride is technically brilliant and an extremely competent guitar player.  A ringer, if you will.  And when it comes to soloing, he is definitely spurring Airey to greater heights.  Airey sounds rejuvenated on this record and has traded in the earthy Hammond B2 tones for a more synthy, lighter, more 'airy' timbre (no pun intended, ok yes it was).  It's a bit jarring, the tone, but the least of our worries.  A lot of the organ vs guitar dueling is really quite exciting, with some good proggy breakdown runs mixed in.  The let down is twofold, in my opinion.  The lack of anything resembling a killer riff, vocal or instrument wise and just a lack luster collection of songs.  After a listen, there's nothing truly memorable about the album.  Nothing that makes you want to go back and have a second listen.  It's not terrible, it's just not great either.  In short, it commits musics greatest sin, it's boring.  And lastly, the production puts Gillans voice above everything else in the mix.  The music could be more accessible if he was blended in with the rest of the band.  Ian isn't the screeching olympiad he used to be.  And he's definitely not Sinatra.  Making him stick out vocally is not doing him or the band any favors</p>
+<span class="rating"> (rating: 2/5 whammy bars)</span>
 
 </main>
 
