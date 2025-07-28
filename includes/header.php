8b3127a3448d8b008f4b22ce75d1724a1eddd9a6
Shannon <sestumpf@gmail.com>
2025-07-28 12:07:52 -0700

    links blocked out in small screen, looks good on all pages

--- includes/header.php
+++ includes/header.php
@@ -21,15 +21,15 @@ $hideLogoPages = ['rainbow.php', 'gillan.php']; // add more as needed
         <li><a href="index.php"><span class='bolded-nav'>Home</span></a></li>
         <li><a href="mark1.php"><span class='bolded-nav'>Mark I</span> - Early Years</a></li>
         <li><a href="mark2.php"><span class='bolded-nav'>Mark II a</span> - Golden Era </a></li>
-        <li><a href="mark3.php"><span class='bolded-nav'>Mark III</span> - Coverdale, Hughes Era</a></li>
+        <li><a href="mark3.php"><span class='bolded-nav'>Mark III</span> - Coverdale, Hughes</a></li>
         <li><a href="mark4.php"><span class='bolded-nav'>Mark IV</span> - Bolin era</a></li>
         <li><a href="mark2_reunion.php"><span class='bolded-nav'>Mark II b</span> - Reunion</a></li>
-        <li><a href="mark5.php"><span class='bolded-nav'>Mark V</span> - Joe Lynn Turner era</a></li>
+        <li><a href="mark5.php"><span class='bolded-nav'>Mark V</span> - Joe Lynn Turner</a></li>
         <li><a href="mark2_3.php"><span class='bolded-nav'>Mark II c</span> - 2nd Reunion</a></li>
         <li><a href="mark6.php"><span class='bolded-nav'>Mark VI</span> - Steve Morse era</a></li>
         <li><a href="mark7.php"><span class='bolded-nav'>Mark VII</span> - Don Airey era</a></li>
-        <li><a href="mark8.php"><span class='bolded-nav'>Mark VIII</span> - Simon McBride era</a></li>
-        <li><a href="rainbow.php"><span class='bolded-nav'>Rainbow</span> - Ritchie Blackmore solo</a></li>
-        <li><a href="gillan.php"><span class='bolded-nav'>Ian Gillan</span> - Ian Gillan Solo</a></li>
+        <li><a href="mark8.php"><span class='bolded-nav'>Mark VIII</span> - Simon McBride</a></li>
+        <li><a href="rainbow.php"><span class='bolded-nav'>Rainbow</span> - Ritchie Blackmore</a></li>
+        <li><a href="gillan.php"><span class='bolded-nav'>Ian Gillan</span> - Ian Gillan</a></li>
     </ul>
 </nav>
